fe81676b3f10e66ccb40af0565d6dcde79adcac3
fmtabbara <fmtabbara@hotmail.co.uk>
2023-08-16 17:29:07 +0100

    update prettier tabs

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
-	return <h1>Hello World</h1>;
+  return <h1>Hello World</h1>;
 };
 
 const domElement = document.getElementById('root');
 if (domElement) {
-	const root = createRoot(domElement);
-	root.render(<App />);
+  const root = createRoot(domElement);
+  root.render(<App />);
 }
