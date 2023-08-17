687d5c03a8669406ad1f99d430b469c272019504
fmtabbara <fmtabbara@hotmail.co.uk>
2023-08-17 10:38:42 +0100

    set up tailwind css

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <h1 className="text-2xl font-bold underline text-blue-500">Hello world!</h1>
+  );
 };
 
 const domElement = document.getElementById('root');
--- server/server.ts
+++ server/server.ts
@@ -5,24 +5,26 @@ import { connectDB } from './config/db';
 
 const app = express();
 
-dotenv.config({path: './config/.env'});
+dotenv.config({ path: './config/.env' });
 
 const runServer = async () => {
-    try{
-        await connectDB();
+  try {
+    await connectDB();
 
-        app.get('/', (_req, res) => res.send('Hello from the server! 🚀🚀🚀'));
-    
-        app.use(logger('dev'));
-    
-        const PORT = process.env.PORT || 2023;
-    
-        app.listen(PORT, () => {
-            console.log(`Sever is running in ${process.env.NODE_ENV} mode & listening on PORT ${PORT}`);
-        });
-    }catch(error) {
-        console.error('Server Error:', error);
-    }
-}
+    app.get('/', (_req, res) => res.send('Hello from the server! 🚀🚀🚀'));
+
+    app.use(logger('dev'));
+
+    const PORT = process.env.PORT || 2023;
+
+    app.listen(PORT, () => {
+      console.log(
+        `Sever is running in ${process.env.NODE_ENV} mode & listening on PORT ${PORT}`
+      );
+    });
+  } catch (error) {
+    console.error('Server Error:', error);
+  }
+};
 
 runServer();
