90453ec09101da512c54e7d26be2828557463c4e
fmtabbara <fmtabbara@hotmail.co.uk>
2023-09-02 12:44:37 +0100

    create stories for IconButton component

---
+++ app/src/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { IconButton } from './IconButton';
+import { InfoIcon } from '../Icons/InfoIcon';
+import { HomeIcon } from '../Icons/HomeIcon';
+import { IconButtonProps } from './types';
+
+export default {
+  title: 'IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+export const InfoSolid: { args: IconButtonProps } = {
+  args: {
+    color: 'primary',
+    Icon: <InfoIcon />,
+    onClick: () => alert('IconButton Clicked'),
+  },
+};
+
+export const InfoSquare: { args: IconButtonProps } = {
+  args: {
+    color: 'secondary',
+    shape: 'square',
+    Icon: <HomeIcon />,
+    onClick: () => alert('IconButton Clicked'),
+  },
+};
+
+export const HomeOutline: { args: IconButtonProps } = {
+  args: {
+    color: 'primary',
+    variant: 'outline',
+    Icon: <HomeIcon />,
+    onClick: () => alert('IconButton Clicked'),
+  },
+};
+
+export const HomeSolidSecondary: { args: IconButtonProps } = {
+  args: {
+    color: 'secondary',
+    Icon: <HomeIcon />,
+    onClick: () => alert('IconButton Clicked'),
+  },
+};
