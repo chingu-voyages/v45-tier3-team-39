dfa35e27798e9e7a2f1e61fd1afbe9acb5df0065
felcard <zundroid@gmail.com>
2023-09-06 23:04:08 +0100

    add navbar

--- app/src/pages/Kitchen/index.tsx
+++ app/src/pages/Kitchen/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Navbar } from 'src/components/Navigation/Navbar/Navbar';
+import { Kitchen } from 'src/components/Icons/Kitchen';
 
 export const KitchenOrders = (): JSX.Element => {
-  return <>HELLO AMIGOS</>;
+  return (
+    <>
+      <Navbar
+        icon={<Kitchen />}
+        title="Kitchen"
+        onClick={() => console.log('clicked')}
+        onClickLogout={() => console.log('logged out')}
+      />
+    </>
+  );
 };
