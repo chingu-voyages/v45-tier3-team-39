3c00667010e80079daf1ed31ce92cbe903fb56c6
felcard <zundroid@gmail.com>
2023-09-06 16:21:02 +0100

    fix syntax erros - auto formatter

--- server/src/routes/menuRoutes.js
+++ server/src/routes/menuRoutes.js
@@ -1,15 +1,19 @@
 const router = require('express').Router();
 
-import { createdMenu, updateMenu, getAllMenu,getOneMenu, deleteMenu} from'../controllers/menuController';
-import { protect, admin } from "../middleware/authMiddleware";
+import {
+  createdMenu,
+  updateMenu,
+  getAllMenu,
+  getOneMenu,
+  deleteMenu,
+} from '../controllers/menuController';
+import { protect, admin } from '../middleware/authMiddleware';
 
-
-router.route("/").post(protect,admin, createdMenu).get(getAllMenu);
+router.route('/').post(protect, admin, createdMenu).get(getAllMenu);
 router
-  .route("/:id")
-  .put(protect, admin,  updateMenu)
-  .delete( protect, admin,deleteMenu)
+  .route('/:id')
+  .put(protect, admin, updateMenu)
+  .delete(protect, admin, deleteMenu)
   .get(getOneMenu);
- 
- 
-  export default router
+
+export default router;
