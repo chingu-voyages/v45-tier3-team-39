a2f20e547cb4d6bd24e5f6952e6c734b78bea94f
felcard <zundroid@gmail.com>
2023-09-11 10:43:42 +0100

    re-factor to use existent components

--- app/src/pages/Kitchen/index.tsx
+++ app/src/pages/Kitchen/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Navbar } from 'src/components/Navigation/Navbar/Navbar';
 import { Kitchen } from 'src/components/Icons/Kitchen';
-import { KitchenTableRow } from 'src/components/Table/KitchenTableRow';
 import { KitchenRowModal } from 'src/components/Table/KitchenRowModal';
+import { Table } from 'src/components/Table';
+import { TableRow } from 'src/components/Table';
 import { ordersState, Order } from 'src/atoms';
 import { orders } from 'src/seeds';
 import { useRecoilState } from 'recoil';
 import { useState, useEffect } from 'react';
+import { DeleteIcon } from 'src/components/Icons/DeleteIcon';
+import { findNumberOfPropertyValuesInArrayOfObjects } from 'src/utils';
+import classes from 'src/components/Badge/classes';
 
 export const KitchenPage = (): JSX.Element => {
   const [restaurantOrders, setRestaurantOrders] = useRecoilState(ordersState);
@@ -14,6 +18,59 @@ export const KitchenPage = (): JSX.Element => {
   useEffect(() => {
     setRestaurantOrders([...restaurantOrders, ...orders]);
   }, []);
+
+  type StatusClass = {
+    ordered: string;
+    preparing: string;
+    ready: string;
+  };
+  const statusClass: StatusClass = {
+    ordered: `badge ${classes.size['lg']} px-8`,
+    preparing: `badge ${classes.size['lg']} ${classes.color['info']} px-6`,
+    ready: `badge ${classes.size['lg']} ${classes.color['warning']} px-10`,
+  };
+
+  const tableRows = restaurantOrders.map((order) => {
+    const categoriesInOrder = findNumberOfPropertyValuesInArrayOfObjects(
+      order.items,
+      'category'
+    );
+
+    return (
+      <TableRow
+        key={order._id}
+        rowClass="select-none"
+        data={[
+          { value: '' },
+          { value: order.table },
+          { value: order._id },
+          {
+            value: (
+              <div
+                className={`cursor-pointer ${
+                  statusClass[order.orderStatus as keyof StatusClass]
+                }`}
+                onClick={() => setSelectedOrder(order)}
+              >
+                {order.orderStatus}
+              </div>
+            ),
+          },
+          { value: categoriesInOrder },
+          {
+            value: (
+              <button
+                className="btn btn-ghost btn-circle"
+                onClick={() => console.log(order._id)}
+              >
+                <DeleteIcon />
+              </button>
+            ),
+          },
+        ]}
+      />
+    );
+  });
 
   return (
     <div>
@@ -24,28 +81,9 @@ export const KitchenPage = (): JSX.Element => {
         onClickLogout={() => console.log('logged out')}
       />
       <div className="overflow-x-auto">
-        <table className="table table-zebra table-pin-rows table-pin-cols">
-          <thead>
-            <tr>
-              <th></th>
-              <th className="pl-0">Table</th>
-              <th>Order</th>
-              <th>Status</th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            {restaurantOrders.map((order) => {
-              return (
-                <KitchenTableRow
-                  key={order._id}
-                  order_id={order._id}
-                  onSelect={() => setSelectedOrder(order)}
-                />
-              );
-            })}
-          </tbody>
-        </table>
+        <Table headers={['', 'Table', 'Order', 'Status', 'Items', 'Delete']}>
+          {tableRows}
+        </Table>
       </div>
       {selectedOrder && (
         <KitchenRowModal
