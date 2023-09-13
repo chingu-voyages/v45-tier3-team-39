1e87b71238de2aec9735dedef9e12762277b8799
felcard <zundroid@gmail.com>
2023-09-13 08:46:23 +0100

    re-factor Stats to make it reusable

---
+++ app/src/components/Stat/classes.ts
@@ -0,0 +1,95 @@
+type StatContClasses = {
+  background: {
+    primary: 'bg-primary';
+    secondary: 'bg-secondary';
+    none: '';
+  };
+  align: {
+    vertical: 'stats vertical';
+    horizontal: 'stats horizontal';
+    responsive: 'lg:stats-horizontal';
+  };
+  justify: {
+    center: 'place-items-center';
+    right: 'text-right';
+  };
+  titleColor: {
+    primary: 'text-primary';
+    secondary: 'text-secondary';
+    none: '';
+  };
+  valueColor: {
+    primary: 'text-primary';
+    secondary: 'text-secondary';
+    none: '';
+  };
+  descriptionColor: {
+    primary: 'text-primary';
+    secondary: 'text-secondary';
+    none: '';
+  };
+  titleSize: {
+    sm: 'stat-desc';
+    md: 'stat-title';
+    lg: 'stat-value';
+  };
+  valueSize: {
+    sm: 'stat-desc';
+    md: 'stat-title';
+    lg: 'stat-value';
+  };
+  descriptionSize: {
+    sm: 'stat-desc';
+    md: 'stat-title';
+    lg: 'stat-value';
+  };
+};
+
+const classes: StatContClasses = {
+  background: {
+    primary: 'bg-primary',
+    secondary: 'bg-secondary',
+    none: '',
+  },
+  align: {
+    vertical: 'stats vertical',
+    horizontal: 'stats horizontal',
+    responsive: 'lg:stats-horizontal',
+  },
+  justify: {
+    center: 'place-items-center',
+    right: 'text-right',
+  },
+  titleColor: {
+    primary: 'text-primary',
+    secondary: 'text-secondary',
+    none: '',
+  },
+  valueColor: {
+    primary: 'text-primary',
+    secondary: 'text-secondary',
+    none: '',
+  },
+  descriptionColor: {
+    primary: 'text-primary',
+    secondary: 'text-secondary',
+    none: '',
+  },
+  titleSize: {
+    sm: 'stat-desc',
+    md: 'stat-title',
+    lg: 'stat-value',
+  },
+  valueSize: {
+    sm: 'stat-desc',
+    md: 'stat-title',
+    lg: 'stat-value',
+  },
+  descriptionSize: {
+    sm: 'stat-desc',
+    md: 'stat-title',
+    lg: 'stat-value',
+  },
+};
+
+export default classes;
--- app/src/components/Stat/types.ts
+++ app/src/components/Stat/types.ts
@@ -1,8 +1,32 @@
-export type StatProps = {
+type StatsProps = {
+  background: Background;
+  align: Alignment;
+  stats: Stat[];
+};
+
+type Stat = {
+  justify?: Justify;
+  figure?: React.ReactNode;
   title: string;
   value: string | number;
-  style?: string;
-  align?: string;
-  titleStyle?: string;
-  valueStyle?: string;
+  description?: string;
+  titleColor?: TitleColor;
+  titleSize?: TitleSize;
+  valueColor?: ValueColor;
+  valueSize?: ValueSize;
+  descriptionColor?: DescriptionColor;
+  descriptionSize?: DescriptionSize;
+  actions?: React.ReactNode;
 };
+
+type Background = 'primary' | 'secondary' | 'none';
+type Alignment = 'vertical' | 'horizontal' | 'responsive';
+type Justify = 'center' | 'right';
+type TitleColor = 'primary' | 'secondary' | 'none';
+type ValueColor = 'primary' | 'secondary' | 'none';
+type DescriptionColor = 'primary' | 'secondary' | 'none';
+type TitleSize = 'sm' | 'md' | 'lg';
+type ValueSize = 'sm' | 'md' | 'lg';
+type DescriptionSize = 'sm' | 'md' | 'lg';
+
+export { StatsProps, Background, Alignment, Justify };
