7ec682d9d76fb020928e908fbc9fc595194d489c
fmtabbara <fmtabbara@hotmail.co.uk>
2023-09-17 14:10:42 +0100

    use basket items in order page

---
+++ app/src/pages/Order/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { NavbarArrow } from '../../components/Navigation/NavbarArrow/NavbarArrow';
+import { OrderTable } from './components/OrderTable';
+import { useRecoilState } from 'recoil';
+import { basketState } from 'src/atoms';
+
+export const OrderPage = () => {
+  const [basketItems, setBaketItems] = useRecoilState(basketState);
+
+  const handleDeleteItem = (id: string) => {
+    console.log('delete item', id);
+
+    const newItems = basketItems.filter((item) => item.id !== id);
+    setBaketItems(newItems);
+  };
+
+  return (
+    <>
+      <NavbarArrow link="/menu" linkName="Back" restaurantName="La mia Pizza" />
+      <div className="overflow-x-auto">
+        <OrderTable items={basketItems} onDelete={handleDeleteItem} />
+      </div>
+    </>
+  );
+};
