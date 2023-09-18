f92d81257df6a2131d1e8efe7ce381c6f1ebf503
fmtabbara <fmtabbara@hotmail.co.uk>
2023-09-18 16:19:29 +0100

    use Icon component

--- app/src/pages/Order/index.tsx
+++ app/src/pages/Order/index.tsx
@@ -4,6 +4,7 @@ import { OrderTable } from './components/OrderTable';
 import { useRecoilState } from 'recoil';
 import { basketState } from '~src/atoms';
 import { Alert } from '~src/components/Alert';
+import { InfoIcon } from '~src/components/Icons/InfoIcon';
 
 export const OrderPage = () => {
   const [basketItems, setBaketItems] = useRecoilState(basketState);
@@ -23,21 +24,7 @@ export const OrderPage = () => {
             title="Basket is empty"
             color="info"
             customClasses="max-w-md m-4"
-            Icon={
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="stroke-current shrink-0 w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                ></path>
-              </svg>
-            }
+            Icon={<InfoIcon />}
           />
         )}
       </div>
