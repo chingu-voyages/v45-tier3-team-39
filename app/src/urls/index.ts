2939eccdf4531a98d18e77baee40b44b78307b2e
fmtabbara <fmtabbara@hotmail.co.uk>
2023-09-22 22:28:36 +0100

    update urls

--- app/src/urls/index.ts
+++ app/src/urls/index.ts
@@ -1,5 +1,7 @@
 const serverURL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:2023' : '';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:2023'
+    : 'https://ordr-ua19.onrender.com';
 const apiURL = `${serverURL}/api`;
 
 export { serverURL, apiURL };
