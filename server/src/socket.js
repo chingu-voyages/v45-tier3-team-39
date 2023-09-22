58ee416d9bf1760d7d043aad0ef066c78997508b
felcard <zundroid@gmail.com>
2023-09-22 11:19:15 +0100

    add cors

--- server/src/socket.js
+++ server/src/socket.js
@@ -1,7 +1,7 @@
 //import { createServer } from 'http';
 import { Server } from 'socket.io';
 //const server = createServer();
-const io = new Server();
+const io = new Server({ cors: { origin: 'https://ordr.onrender.com/' } });
 
 io.on('connection', (socket) => {
   console.log('client connected via SOCKET');
