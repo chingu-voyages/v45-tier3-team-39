be9fd68d4f4140c142bc6caa59535a66eb4b6d9f
felcard <zundroid@gmail.com>
2023-10-05 13:02:34 +0100

    enable category selection

--- app/src/pages/Menu/index.tsx
+++ app/src/pages/Menu/index.tsx
@@ -6,12 +6,17 @@ import MenuCard from '~src/components/Card/MenuCard';
 import { Badge } from '~src/components/Badge/Badge';
 import { CardModal } from '~src/components/Modals/CardModal';
 import { basketState } from '~src/atoms';
-import { MenuItem } from '@ordr/types';
+import { MenuItem, Category } from '@ordr/types';
 import { apiURL } from '~src/urls';
+import { Navbar } from '~src/components/Navigation/Navbar/Navbar';
+import { RestaurantIcon } from '~src/components/Icons/RestaurantIcon';
+import OrdrLoading from '~src/components/Loading/OrdrLoading';
 
 export const MenuPage = (): JSX.Element => {
   const [selectedItem, setSelectedItem] = useState<MenuItem>();
   const [menuItems, setMenuItems] = useState<MenuItem[]>(); // [
+  const [selectCategoryItems, setSelectCategoryItems] = useState<MenuItem[]>();
+  const [categories, setCategories] = useState<string[]>();
   const basket = useRecoilValue(basketState);
 
   const fetchMenu = async () => {
@@ -20,37 +25,78 @@ export const MenuPage = (): JSX.Element => {
     return json;
   };
 
+  const fetchCategories = async () => {
+    const res = (await fetch(`${apiURL}/categories`)).json();
+    return res;
+  };
+
   useEffect(() => {
-    fetchMenu().then(setMenuItems).catch(console.error);
+    fetchMenu()
+      .then((res) => {
+        setMenuItems(res);
+        setSelectCategoryItems(res);
+      })
+      .catch(console.error);
+    fetchCategories()
+      .then((res) => {
+        const categoryNames = res.map(
+          (categoryName: Category) => categoryName.name
+        );
+        const addAllCategory = [...categoryNames, 'all'];
+        setCategories(addAllCategory);
+      })
+      .catch(console.error);
   }, []);
+
+  const onSetCategory = (cat: string) => {
+    if (cat === 'all') {
+      setSelectCategoryItems(menuItems);
+    } else {
+      const categoryItems = menuItems?.filter(
+        (item) => item.category.name === cat
+      );
+      setSelectCategoryItems(categoryItems);
+    }
+  };
 
   const basketUnits =
     basket.length > 0
       ? basket.reduce((prev, curr) => prev + curr.quantity, 0)
       : 0;
 
+  if (!menuItems || !categories) {
+    return <OrdrLoading />;
+  }
+
   return (
     <div>
-      <div className="navbar p-4">
-        <div className="navbar-start">
-          <CatDropdown onSetCategory={() => {}} />
-        </div>
-        <div className="navbar-center">
-          <h1 className="text-xl font-bold">La mia Pizza</h1>
-        </div>
-        <div className="navbar-end indicator">
-          <Badge
-            label={basket.length > 0 ? basketUnits.toString() : '0'}
-            customClasses="indicator-item"
-          />
-          <Link role="button" className="btn" to="/order">
-            Your order
-          </Link>
-        </div>
-      </div>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 p-4">
+      <Navbar
+        menu={
+          categories && (
+            <CatDropdown
+              categories={categories}
+              onSetCategory={onSetCategory}
+            />
+          )
+        }
+        icon={<RestaurantIcon />}
+        title={'La Mia Pizza'}
+        onClickLogout={() => console.log('logged out')}
+        action={
+          <div className="navbar-end indicator">
+            <Badge
+              label={basket.length > 0 ? basketUnits.toString() : '0'}
+              customClasses="indicator-item"
+            />
+            <Link role="button" className="btn" to="/order">
+              Your order
+            </Link>
+          </div>
+        }
+      />
+      <div className="sm:mt-16 mt-40 grid grid-cols-2 md:grid-cols-4 gap-6 p-4">
         {menuItems &&
-          menuItems.map((item) => {
+          selectCategoryItems?.map((item) => {
             return (
               <MenuCard
                 name={item.name}
